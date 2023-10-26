871eb4e2281c46fc3f730924a97d128647d379cc
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2023-10-26 21:19:54 +1300

    Updated dependency and sheep texture.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Fleecifer (Quilt)
+
+**WARNING:** You may experience issues when running with other mods that change sheep behaviour.
+
+## About
+Fleecifer is a Minecraft mod developed for the Quilt Loader. This mod replaces sheep behaviour, turning them hostile towards the player.
+
+## Mod Requirements
+You only need the [Quilt](https://quiltmc.org/) Loader.  
---
+++ src/main/java/com/mclegoman/fleecifer/mixin/SheepEntityMixin.java
@@ -0,0 +1,29 @@
+package com.mclegoman.fleecifer.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.RevengeGoal;
+import net.minecraft.entity.ai.goal.TargetGoal;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SheepEntity.class)
+public abstract class SheepEntityMixin extends AnimalEntity {
+	protected SheepEntityMixin(EntityType<? extends AnimalEntity> entityType, World world) {
+		super(entityType, world);
+	}
+	@Inject(method = "initGoals", at = @At("RETURN"))
+	private void initGoals(CallbackInfo ci) {
+		this.getAttributeInstance(EntityAttributes.GENERIC_ARMOR).setBaseValue(8.0);
+		this.targetSelector.add(0, new TargetGoal<>(this, PlayerEntity.class, true));
+		this.goalSelector.add(0, new MeleeAttackGoal(this, 1.25, true));
+		this.goalSelector.add(1, new RevengeGoal(this).setGroupRevenge(SheepEntity.class));
+	}
+}
